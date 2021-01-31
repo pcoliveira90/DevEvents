6d0638121f75689a59fcea4df82d647494b1a227
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-31 16:01:38 -0300

    initial entity

---
+++ DevEvents.API/Entities/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevEvents.API.Entities
+{
+   public class Event
+   {
+      public int id { get; set; }
+
+
+      public string title { get; set; }
+
+      public string description { get; set; }
+
+      public bool active { get; set; }
+
+      public DateTime dateStart { get; set; }
+
+      public DateTime dateEnd { get; set; }
+
+      public DateTime dateRegister { get; set; }
+   }
+}
---
+++ DevEvents.API/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevEvents.API.Entities
+{
+   public class User
+   {
+      public int id { get; set; }
+
+      public string username { get; set; }
+
+      public DateTime birthDate { get; set; }
+
+      public string email { get; set; }
+
+      public DateTime dateRegister { get; set; }
+
+      public bool active { get; set; }
+
+   }
+}
