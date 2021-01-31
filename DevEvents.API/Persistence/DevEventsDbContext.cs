9d27e13f986ad4c373e630053e1521b6d4ab84e9
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-31 17:34:07 -0300

    relationship

--- DevEvents.API/Persistence/DevEventsDbContext.cs
+++ DevEvents.API/Persistence/DevEventsDbContext.cs
@@ -22,6 +22,26 @@ namespace DevEvents.API.Persistence
 
       protected override void OnModelCreating(ModelBuilder modelBuilder)
       {
+         
+         modelBuilder.Entity<Event>().ToTable("tb_event");
+         modelBuilder.Entity<Event>().HasKey(e => e.id);
+
+         modelBuilder.Entity<Event>()
+            .HasOne(e => e.category)
+            .WithMany()
+            .HasForeignKey(e => e.idCategory);
+
+         modelBuilder.Entity<Event>()
+            .HasOne(e => e.user)
+            .WithMany()
+            .HasForeignKey(e => e.idUser);
+
+
+         modelBuilder.Entity<User>().ToTable("tb_user");
+         modelBuilder.Entity<User>().HasKey(e => e.id);
+
+         modelBuilder.Entity<Category>().ToTable("tb_category");
+         modelBuilder.Entity<Category>().HasKey(e => e.id);
 
       }
    }
