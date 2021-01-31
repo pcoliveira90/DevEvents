fed47792b5270cab1c33dcd751df56fe46e5f632
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-31 17:04:30 -0300

    DbContext

---
+++ DevEvents.API/Persistence/DevEventsDbContext.cs
@@ -0,0 +1,28 @@
+﻿using DevEvents.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevEvents.API.Persistence
+{
+   public class DevEventsDbContext : DbContext
+   {
+      public DevEventsDbContext(DbContextOptions<DevEventsDbContext> options) : base(options)
+      {
+
+      }
+
+      public DbSet<Event> Events { get; set; }
+
+      public DbSet<Category> Categories { get; set; }
+
+      public DbSet<User> Users { get; set; }
+
+      protected override void OnModelCreating(ModelBuilder modelBuilder)
+      {
+
+      }
+   }
+}
