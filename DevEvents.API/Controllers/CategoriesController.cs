6efd86ea14f3c564a5503fa9c586fd54c5f3bc39
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-31 16:39:02 -0300

    controllers

---
+++ DevEvents.API/Controllers/CategoriesController.cs
@@ -0,0 +1,43 @@
+﻿using DevEvents.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevEvents.API.Controllers
+{
+   [Route("api/[controller]")]
+   public class CategoriesController : ControllerBase
+   {
+      [HttpGet]
+      public IActionResult Get()
+      {
+         return Ok();
+      }
+
+      [HttpGet("{id}")]
+      public IActionResult Get(int id)
+      {
+         return Ok();
+      }
+
+      [HttpDelete("{id}")]
+      public IActionResult Delete(int id)
+      {
+         return Ok();
+      }
+
+      [HttpPost]
+      public IActionResult Insert([FromBody] Category obj)
+      {
+         return Ok();
+      }
+
+      [HttpPut("{id}")]
+      public IActionResult Update(int id, [FromBody] Category obj)
+      {
+         return NoContent();
+      }
+   }
+}
